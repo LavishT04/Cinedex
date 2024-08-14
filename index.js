e4e94e09e510c9f018fa558463b86ffddd3f5011
Lavish Tandon <lavishtandon04@gmail.com>
2024-08-14 16:43:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const form1EL = document.querySelector('#form1');
+const emailInput = document.querySelector('#exampleInputEmail1');
+
+console.log(emailInputValue.value);
+
+form1EL.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('URL')
+
+}
+)
+
+
